f8eceaacbcd168ac6ffe883cff87c96b345615a3
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2024-06-19 13:55:24 -0500

    testing 17 out of 17 for the unit tests before fixing

--- candidate-testing.js
+++ candidate-testing.js
@@ -33,9 +33,12 @@ function askQuestion() {
     console.log("That is incorrect");
   }
     */
+ 
    for(let i = 0; i < questions.length; i++){
+
     candidateAnswers[i] = input.question(questions[i]);
-  
+   // temp = candidateAnswers[i].split("");
+    console.log(candidateAnswers);
    }
 
 }
@@ -45,10 +48,32 @@ function gradeQuiz(candidateAnswers) {
 //reset score
 let correctQuestions = 0;
 
-//loop through answers
+//format answers
+let temp = "";
+//console.log("Before: " + candidateAnswers);
+for(let i = 0; i < candidateAnswers.length; i++){
+   temp = "";
+   //figure out the question your on and do things
+    if(i ===0){
+        //capitalize the first letter of each word and slice the rest on lowercase
+        temp += candidateAnswers[i][0].toUpperCase() + candidateAnswers[i].slice(1,6).toLowerCase();
+        temp += candidateAnswers[i].charAt(candidateAnswers[i].indexOf(" ") + 1).toUpperCase() + candidateAnswers[i].slice(7);
+
+    }else if(i ===1){
+        //lower case all the letters
+        temp += candidateAnswers[i].toLowerCase();
+    }else if(i ===3){
+        //capitalize the first letter and lowercase the rest.
+        temp += candidateAnswers[i][0].toUpperCase() + candidateAnswers[i].slice(1).toLowerCase();
+    }else{
+        temp = candidateAnswers[i];
+    }
+    candidateAnswers[i] = temp;
+}//end of format loop
+  
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  for(let i =0; i<candidateAnswers.length; i++){
+  for(let i = 0; i < questions.length; i++){
     if(candidateAnswers[i] === correctAnswers[i]){
       console.log(`You answered question number ${i+1} Correctly`);
       correctQuestions++;
